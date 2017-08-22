0003e89097e48bb1c8f38c50f307db5664371837
blenderskool <akash.hamirwasia@gmail.com>
2017-08-22 21:44:41 +0530

    Old Links redirection fix

--- resources/js/head_script.js
+++ resources/js/head_script.js
@@ -4,3 +4,9 @@ window.$crisp=[];window.CRISP_WEBSITE_ID="4c3564b5-198e-4b32-b622-3b1ef3d545db";
 	s.src="https://client.crisp.chat/l.js";
 	s.async=1;d.getElementsByTagName("head")[0].appendChild(s);
 })();
+
+// if old URL is detected
+if (window.location.pathname == '/kaleidoscope.html')
+	window.location.pathname = '/pages/kaleidoscope.html';
+else if (window.location.pathname == '/enrich.html')
+	window.location.pathname = '/pages/enrich.html';
