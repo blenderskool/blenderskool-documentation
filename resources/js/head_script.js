6c0db3a86d7a0d37ca89ecbaf4deb22638c1fc01
blenderskool <akash.hamirwasia@gmail.com>
2017-08-22 20:02:41 +0530

    Version 2.0 of site. Not uses Mobirise anymore

---
+++ resources/js/head_script.js
@@ -0,0 +1,6 @@
+window.$crisp=[];window.CRISP_WEBSITE_ID="4c3564b5-198e-4b32-b622-3b1ef3d545db";
+(function(){
+	d=document;s=d.createElement("script");
+	s.src="https://client.crisp.chat/l.js";
+	s.async=1;d.getElementsByTagName("head")[0].appendChild(s);
+})();
---
+++ resources/js/script.js
@@ -0,0 +1,59 @@
+function back_to_top() {
+	$("html, body").animate({ scrollTop: 0 }, "slow");
+}
+
+function open_mobile_menu() {
+	if ($('.navigation .menu-wrap').css("visibility") === "visible")
+	{
+		$('.navigation .menu-wrap').css({'visibility': 'hidden', 'opacity': 0});  // Close the Menu
+		$('.navigation').css({'background-color': 'rgba(0, 0, 0, 0)'})
+	}
+	else
+	{
+		$('.navigation .menu-wrap').css({'visibility': 'visible', 'opacity': 1}); // Open the Menu
+		$('.navigation').css({'background-color': 'black'})
+	}
+}
+
+$(window).on('scroll', function() {
+	var y_scroll_pos = window.pageYOffset;
+	var scroll_pos_test = 0;
+
+	if (($('.navigation .menu-wrap').css("visibility") === "hidden") || $('.navigation .mobile-menu').css("display") === "none")
+	{
+		if(y_scroll_pos > scroll_pos_test)
+			$('.navigation').css({'background-color': 'rgba(0, 0, 0, 0.3)', 'padding': "5px 100px"});
+		else if (y_scroll_pos==0)
+			$('.navigation').css({'background-color': 'rgba(0, 0, 0, 0)', 'padding': "10px 100px"});
+	}
+	else
+		$('.navigation').css({'background-color': 'black', 'padding': "5px 100px"});
+
+	if (y_scroll_pos > 0.65*window.screen.availHeight)
+		$('.to-top-container').css({'opacity': '1'});
+	else
+		$('.to-top-container').css({'opacity': '0'});
+
+});
+
+window.onload = function(e){
+    if (window.location.pathname.includes("enrich"))
+    {
+    	$('.latest-bar').css({'display': 'none'});  // Do not show Enrich Promo on Enrich pages
+    	$('.navigation .mobile-menu').css({'top': '30px'});
+    }
+    else
+    	$('.navigation .mobile-menu').css({'top': '65px'});
+
+	$('.crisp-client').css({'opacity': '1'})
+}
+
+if (!!document.getElementById('btnGetStarted')) {
+	document.getElementById('btnGetStarted').addEventListener('click', function(e) {
+			event.preventDefault();
+
+			$('html, body').animate({
+					scrollTop: $('#get-started').offset().top
+			}, 500);
+	});
+}
